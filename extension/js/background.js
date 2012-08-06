fe6c952c9ed2e66d112218e6799c517998c6ae52
callum85 <callum.locke@gmail.com>
2012-08-06 10:15:08 +0100

    version number to 0.5.4

--- extension/js/background.js
+++ extension/js/background.js
@@ -129,7 +129,7 @@
     // }
 
   // Record current version (in case future update wants to know)
-    localStorage.jfVersion = '0.5.3' ;
+    localStorage.jfVersion = '0.5.4' ;
 
   // Template elements
     var templates,
