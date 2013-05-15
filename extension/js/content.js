ec284a33a0f5a8b08c0664c9e08a13c5107fe1ab
iflamed <iflamed@gmail.com>
2013-05-15 18:44:42 +0800

    Add a button to view the beautify json source
    For issue #17

--- extension/js/content.js
+++ extension/js/content.js
@@ -39,6 +39,7 @@
 
   var jfContent,
       pre,
+      beautifyPre,
       jfStyleEl,
       slowAnalysisTimeout,
       port,
@@ -62,6 +63,7 @@
           pre.hidden = false ;
           // console.log('Unhidden the PRE') ;
           document.body.removeChild(jfContent) ;
+          document.body.removeChild(beautifyPre) ;
           exitedNotJsonTime = +(new Date()) ;
           break ;
           
@@ -81,7 +83,7 @@
 
             jfStyleEl.insertAdjacentHTML(
               'beforeend',
-              'body{-webkit-user-select:text;overflow-y:scroll !important;margin:0;position:relative}#optionBar{-webkit-user-select:none;display:block;position:absolute;top:9px;right:17px}#buttonFormatted,#buttonPlain{-webkit-border-radius:2px;-webkit-box-shadow:0px 1px 3px rgba(0,0,0,0.1);-webkit-user-select:none;background:-webkit-linear-gradient(#fafafa, #f4f4f4 40%, #e5e5e5);border:1px solid #aaa;color:#444;font-size:12px;margin-bottom:0px;min-width:4em;padding:3px 0;position:relative;z-index:10;display:inline-block;width:80px;text-shadow:1px 1px rgba(255,255,255,0.3)}#buttonFormatted{margin-left:0;border-top-left-radius:0;border-bottom-left-radius:0}#buttonPlain{margin-right:0;border-top-right-radius:0;border-bottom-right-radius:0;border-right:none}#buttonFormatted:hover,#buttonPlain:hover{-webkit-box-shadow:0px 1px 3px rgba(0,0,0,0.2);background:#ebebeb -webkit-linear-gradient(#fefefe, #f8f8f8 40%, #e9e9e9);border-color:#999;color:#222}#buttonFormatted:active,#buttonPlain:active{-webkit-box-shadow:inset 0px 1px 3px rgba(0,0,0,0.2);background:#ebebeb -webkit-linear-gradient(#f4f4f4, #efefef 40%, #dcdcdc);color:#333}#buttonFormatted.selected,#buttonPlain.selected{-webkit-box-shadow:inset 0px 1px 5px rgba(0,0,0,0.2);background:#ebebeb -webkit-linear-gradient(#e4e4e4, #dfdfdf 40%, #dcdcdc);color:#333}#jsonpOpener,#jsonpCloser{padding:4px 0 0 8px;color:black;margin-bottom:-6px}#jsonpCloser{margin-top:0}#formattedJson{padding-left:28px;padding-top:6px}pre{padding:36px 5px 5px 5px}.kvov{display:block;padding-left:20px;margin-left:-20px;position:relative}.collapsed{white-space:nowrap}.collapsed>.blockInner{display:none}.collapsed>.ell:after{content:"…";font-weight:bold}.collapsed>.ell{margin:0 4px;color:#888}.collapsed .kvov{display:inline}.e{width:20px;height:18px;display:block;position:absolute;left:-2px;top:1px;z-index:5;background-image:url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAYAAADED76LAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAD1JREFUeNpiYGBgOADE%2F3Hgw0DM4IRHgSsDFOzFInmMAQnY49ONzZRjDFiADT7dMLALiE8y4AGW6LoBAgwAuIkf%2F%2FB7O9sAAAAASUVORK5CYII%3D");background-repeat:no-repeat;background-position:center center;display:block;opacity:0.15}.collapsed>.e{-webkit-transform:rotate(-90deg);width:18px;height:20px;left:0px;top:0px}.e:hover{opacity:0.35}.e:active{opacity:0.5}.collapsed .kvov .e{display:none}.blockInner{display:block;padding-left:24px;border-left:1px dotted #bbb;margin-left:2px}#formattedJson,#jsonpOpener,#jsonpCloser{color:#333;font:13px/18px monospace}#formattedJson{color:#444}.b{font-weight:bold}.s{color:#0B7500;word-wrap:break-word}a:link,a:visited{text-decoration:none;color:inherit}a:hover,a:active{text-decoration:underline;color:#050}.bl,.nl,.n{font-weight:bold;color:#1A01CC}.k{color:black}#formattingMsg{font:13px "Lucida Grande", "Segoe UI", "Tahoma";padding:10px 0 0 8px;margin:0;color:#333}#formattingMsg>svg{margin:0 7px;position:relative;top:1px}[hidden]{display:none !important}span{white-space:pre-wrap}@-webkit-keyframes spin{from{-webkit-transform:rotate(0deg)}to{-webkit-transform:rotate(360deg)}}#spinner{-webkit-animation:spin 2s 0 infinite}*{-webkit-font-smoothing:antialiased}'
+              'body{-webkit-user-select:text;overflow-y:scroll !important;margin:0;position:relative}#optionBar{-webkit-user-select:none;display:block;position:absolute;top:9px;right:17px}#buttonFormatted,#buttonPlain,#buttonBeautified{-webkit-border-radius:2px;-webkit-box-shadow:0px 1px 3px rgba(0,0,0,0.1);-webkit-user-select:none;background:-webkit-linear-gradient(#fafafa, #f4f4f4 40%, #e5e5e5);border:1px solid #aaa;color:#444;font-size:12px;margin-bottom:0px;min-width:4em;padding:3px 0;position:relative;z-index:10;display:inline-block;width:80px;text-shadow:1px 1px rgba(255,255,255,0.3)}#buttonBeautified{-webkit-border-radius:1px;-moz-border-radius:1px;border-radius:1px}#buttonFormatted{margin-left:0;border-top-left-radius:0;border-bottom-left-radius:0}#buttonPlain{margin-right:0;border-top-right-radius:0;border-bottom-right-radius:0;border-right:none}#buttonFormatted:hover,#buttonPlain:hover,#buttonBeautified:hover{-webkit-box-shadow:0px 1px 3px rgba(0,0,0,0.2);background:#ebebeb -webkit-linear-gradient(#fefefe, #f8f8f8 40%, #e9e9e9);border-color:#999;color:#222}#buttonFormatted:active,#buttonPlain:active,#buttonBeautified:active{-webkit-box-shadow:inset 0px 1px 3px rgba(0,0,0,0.2);background:#ebebeb -webkit-linear-gradient(#f4f4f4, #efefef 40%, #dcdcdc);color:#333}#buttonFormatted.selected,#buttonPlain.selected,#buttonBeautified.selected{-webkit-box-shadow:inset 0px 1px 5px rgba(0,0,0,0.2);background:#ebebeb -webkit-linear-gradient(#e4e4e4, #dfdfdf 40%, #dcdcdc);color:#333}#jsonpOpener,#jsonpCloser{padding:4px 0 0 8px;color:#000;margin-bottom:-6px}#jsonpCloser{margin-top:0}#formattedJson{padding-left:28px;padding-top:6px}pre{padding:36px 5px 5px 5px}.kvov{display:block;padding-left:20px;margin-left:-20px;position:relative}.collapsed{white-space:nowrap}.collapsed>.blockInner{display:none}.collapsed>.ell:after{content:"…";font-weight:bold}.collapsed>.ell{margin:0 4px;color:#888}.collapsed .kvov{display:inline}.e{width:20px;height:18px;display:block;position:absolute;left:-2px;top:1px;z-index:5;background-image:url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAYAAADED76LAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAD1JREFUeNpiYGBgOADE%2F3Hgw0DM4IRHgSsDFOzFInmMAQnY49ONzZRjDFiADT7dMLALiE8y4AGW6LoBAgwAuIkf%2F%2FB7O9sAAAAASUVORK5CYII%3D");background-repeat:no-repeat;background-position:center center;display:block;opacity:0.15}.collapsed>.e{-webkit-transform:rotate(-90deg);width:18px;height:20px;left:0px;top:0px}.e:hover{opacity:0.35}.e:active{opacity:0.5}.collapsed .kvov .e{display:none}.blockInner{display:block;padding-left:24px;border-left:1px dotted #bbb;margin-left:2px}#formattedJson,#jsonpOpener,#jsonpCloser{color:#333;font:13px/18px monospace}#formattedJson{color:#444}.b{font-weight:bold}.s{color:#0b7500;word-wrap:break-word}a:link,a:visited{text-decoration:none;color:inherit}a:hover,a:active{text-decoration:underline;color:#050}.bl,.nl,.n{font-weight:bold;color:#1a01cc}.k{color:#000}#formattingMsg{font:13px "Lucida Grande","Segoe UI","Tahoma";padding:10px 0 0 8px;margin:0;color:#333}#formattingMsg>svg{margin:0 7px;position:relative;top:1px}[hidden]{display:none !important}span{white-space:pre-wrap}@-webkit-keyframes spin{from{-webkit-transform:rotate(0deg)}to{-webkit-transform:rotate(360deg)}}#spinner{-webkit-animation:spin 2s 0 infinite}*{-webkit-font-smoothing:antialiased}'
             ) ;
   
             // Add custom font name if set - FROM FUTURE
@@ -123,25 +125,30 @@
           
           // Create toggleFormat button
             var buttonPlain = document.createElement('button'),
-              buttonFormatted = document.createElement('button') ;
+              buttonFormatted = document.createElement('button'),
+              buttonBeautified = document.createElement('button'); ;
             buttonPlain.id = 'buttonPlain' ;
             buttonPlain.innerText = 'Raw' ;
+            buttonBeautified.id = 'buttonBeautified';
+            buttonBeautified.innerText = 'Beautified';
             buttonFormatted.id = 'buttonFormatted' ;
             buttonFormatted.innerText = 'Parsed' ;
             buttonFormatted.classList.add('selected') ;
             
-            var plainOn = false ;
+            var plainOn = 3 ;
             buttonPlain.addEventListener(
               'click',
               function () {
                 // When plain button clicked...
-                if (!plainOn) {
-                  plainOn = true ;
+                if (plainOn != 1) {
+                  plainOn = 1 ;
                   pre.hidden = false ;
                   jfContent.hidden = true ;
+                  beautifyPre.hidden = true;
 
                   buttonFormatted.classList.remove('selected') ;
                   buttonPlain.classList.add('selected') ;
+                  buttonBeautified.classList.remove('selected');
                 }
               },
               false
@@ -151,20 +158,42 @@
               'click',
               function () {
                 // When formatted button clicked...
-                if (plainOn) {
-                  plainOn = false ;
+                if (plainOn != 3) {
+                  plainOn = 3 ;
                   pre.hidden = true ;
                   jfContent.hidden = false ;
+                  beautifyPre.hidden = true;
 
                   buttonFormatted.classList.add('selected') ;
                   buttonPlain.classList.remove('selected') ;
+                  buttonBeautified.classList.remove('selected');
                 }
               },
               false
             ) ;
+
+            //Add beautified event #issue 17
+            buttonBeautified.addEventListener(
+              'click',
+              function () {
+                //when beautify button clicked...
+                if(plainOn !=2){
+                  plainOn = 2;
+                  pre.hidden = true;
+                  beautifyPre.hidden = false;
+                  jfContent.hidden = true;
+
+                  buttonFormatted.classList.remove('selected') ;
+                  buttonPlain.classList.remove('selected') ;
+                  buttonBeautified.classList.add('selected');
+                }
+              },
+              false
+            );
             
             // Put it in optionBar
               optionBar.appendChild(buttonPlain) ;
+              optionBar.appendChild(buttonBeautified);
               optionBar.appendChild(buttonFormatted) ;
 
           // Attach event handlers
@@ -197,6 +226,7 @@
               var script = document.createElement("script") ;
               script.innerHTML = 'window.json = ' + JSON.parse(JSON.stringify(msg[2])) + ';' ;
               document.head.appendChild(script) ;
+              beautifyPre.innerText = JSON.stringify(JSON.parse(msg[2]),null,4);
               console.log('JSON Formatter: Type "json" to inspect.') ;
             }, 100) ;
 
@@ -244,6 +274,10 @@
         
         // Send the contents of the PRE to the BG script
           // Add jfContent DIV, ready to display stuff
+            beautifyPre = document.createElement('pre');
+            beautifyPre.id = 'beautifypre';
+            beautifyPre.hidden = true;
+            document.body.appendChild(beautifyPre) ;
             jfContent = document.createElement('div') ;
             jfContent.id = 'jfContent' ;
             document.body.appendChild(jfContent) ;
